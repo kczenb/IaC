c75ae9a4ad2e9bcba210357e6afdee585f3ccb51
Mac <mac@node-2.local>
2024-10-27 21:09:16 +0800

    change server

--- terraform/main.tf
+++ terraform/main.tf
@@ -33,14 +33,15 @@ resource "aws_instance" "app_instance" {
   # Optional: Add user_data script to install Node.js and start your app
   user_data = <<-EOF
               #!/bin/bash
-              sudo yum update -y
-              sudo yum install -y git
-              sudo yum install -y nodejs npm
-              # Clone and start the app
+              sudo apt update -y
+              sudo apt install -y git
+              sudo apt install -y nodejs npm
+              
+              # Clone the repository and start the app
               git clone https://github.com/kczenb/IaC.git /home/ubuntu/app
               cd /home/ubuntu/app
               npm install
-              npm start &
+              nohup npm start &
               EOF
 }
 
