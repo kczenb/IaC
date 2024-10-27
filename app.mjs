e10706ee4217685b89f1eaf557ea61448ddab4c7
Mac <mac@node-2.local>
2024-10-27 20:55:57 +0800

    change server

--- app.mjs
+++ app.mjs
@@ -7,9 +7,8 @@ app.get('/', (req, res) => {
 });
 
 const PORT = 3000;
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
 
-export default app; // Export for testing
-
+export { app, server };
--- test/app.test.mjs
+++ test/app.test.mjs
@@ -1,11 +1,14 @@
 import request from 'supertest';
 import { expect } from 'chai';
-import app from '../app.mjs';
+import { app, server } from '../app.mjs';
 
 describe('GET /', () => {
   it('should return "Hello Nottingham"', async () => {
     const res = await request(app).get('/');
     expect(res.text).to.equal('Hello Nottingham');
   });
-});
 
+  after(() => {
+    server.close(); // Close the server after tests
+  });
+});
