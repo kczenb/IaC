fafdf8efcdc1081150bb27b50dc2da39c659029c
Mac <mac@node-2.local>
2024-10-27 21:04:15 +0800

    change server

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,9 +2,29 @@ provider "aws" {
   region = "us-east-1" 
 }
 
+resource "aws_security_group" "node_app_sg" {
+  name        = "node_app_security_group"
+  description = "Allow inbound traffic on port 3000"
+
+  ingress {
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # Open to all IPs; restrict as needed
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"  # Allows all outbound traffic
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 resource "aws_instance" "app_instance" {
   ami           = "ami-0866a3c8686eaeeba"  
-  instance_type = "t2.micro"               
+  instance_type = "t2.micro"
+  security_groups = [aws_security_group.node_app_sg.name]               
 
   tags = {
     Name = "NodeApp"
